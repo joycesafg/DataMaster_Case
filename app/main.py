eb8294f02b55142ea69f17e35c6a1d760f1a8d27
Joyce Sa <joycesafg@gmail.com>
2024-07-20 22:13:49 -0300

    primeiro commit

---
+++ app/main.py
@@ -0,0 +1,35 @@
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+import numpy as np
+import uvicorn
+import pickle
+import os
+import xgboost
+
+print(os.listdir("."))
+# Carregar o modelo
+with open('model.pkl', 'rb') as f:
+    model = pickle.load(f)
+
+
+# Iniciar a aplicação FastAPI
+app = FastAPI()
+
+# Definir a estrutura do payload da solicitação
+class IrisRequest(BaseModel):
+    sepal_length: float
+    sepal_width: float
+    petal_length: float
+    petal_width: float
+
+# Endpoint para previsões
+@app.post("/predict")
+def predict(iris: IrisRequest):
+    data = np.array([[iris.sepal_length, iris.sepal_width, iris.petal_length, iris.petal_width]])
+    prediction = model.predict(data)
+    return {"prediction": int(prediction[0])}
+
+# if __name__=="__main__":
+#     app.run(debug=True)
+    #uvicorn.run(app, host="127.0.0.1", port=8001)
